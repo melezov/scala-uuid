e55e2142e587062e38fe0b99e6ee3c79b4768bca
Marko Elezović <marko@element.hr>
2013-09-19 23:25:54 +0200

    Since underlying value in implicit value classes becomes a local variable in that class methods, this is cheaper.

--- src/main/scala/io/jvm/uuid/RichUUID.scala
+++ src/main/scala/io/jvm/uuid/RichUUID.scala
@@ -2,12 +2,12 @@ package io.jvm
 package object uuid extends Imports {
 
 implicit class RichUUID private[uuid] (val uuid: java.util.UUID) extends AnyVal {
-  def leastSigBits: Long = uuid.getLeastSignificantBits
   def mostSigBits: Long = uuid.getMostSignificantBits
+  def leastSigBits: Long = uuid.getLeastSignificantBits
 
   def byteArray: Array[Byte] = {
-    val msb = mostSigBits
-    val lsb = leastSigBits
+    val msb = uuid.getMostSignificantBits
+    val lsb = uuid.getLeastSignificantBits
 
     Array(
       (msb >>> 56).toByte
